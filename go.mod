6909d0ffdca84a15a36a806fd6a2abde3c18aea1
livingstone23 <livingstone23@gmail.com>
2023-06-19 21:48:34 +0200

    Adding Upload Image

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/aws/aws-lambda-go v1.41.0 // indirect
+	github.com/aws/aws-sdk-go v1.44.285 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.18.0 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.18.25 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.13.24 // indirect
@@ -19,6 +20,7 @@ require (
 	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -28,6 +30,6 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.mongodb.org/mongo-driver v1.11.7 // indirect
 	golang.org/x/crypto v0.10.0 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/text v0.10.0 // indirect
 )
---
+++ routers/uploadImage.go
@@ -0,0 +1,120 @@
+package routers
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime"
+	"mime/multipart"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/livingstone23/twittoraws/bd"
+	"github.com/livingstone23/twittoraws/models"
+)
+
+// Estructura necesaria para subir archivo
+type readSeeker struct {
+	io.Reader
+}
+
+// Funcion para subir via lanmbda
+func (rs *readSeeker) Seek(offset int64, whence int) (int64, error) {
+	return 0, nil
+}
+
+func UploadImage(ctx context.Context, uploadType string, request events.APIGatewayProxyRequest, claim models.Claim) models.RespApi {
+	fmt.Println("Ingresando en funcion UploadImage")
+	var r models.RespApi
+	r.Status = 400
+
+	IDUsuario := claim.ID.Hex()
+
+	var filename string
+	var usuario models.Usuario
+	bucket := aws.String(ctx.Value(models.Key("bucketName")).(string))
+
+	switch uploadType {
+	case "A":
+		filename = "avatars/" + IDUsuario + ".jpg"
+		usuario.Avatar = filename
+	case "B":
+		filename = "banners/" + IDUsuario + ".jpg"
+		usuario.Avatar = filename
+	}
+
+	mediaType, params, err := mime.ParseMediaType(request.Headers["Content-Type"])
+	if err != nil {
+		r.Status = 500
+		r.Message = err.Error()
+		return r
+	}
+
+	if strings.HasPrefix(mediaType, "multipart/") {
+		body, err := base64.StdEncoding.DecodeString(request.Body)
+		if err != nil {
+			r.Status = 500
+			return r
+		}
+
+		mr := multipart.NewReader(bytes.NewReader(body), params["boundary"])
+		p, err := mr.NextPart()
+		if err != nil && err != io.EOF {
+			r.Status = 500
+			r.Message = err.Error()
+			return r
+		}
+
+		if err != io.EOF {
+			if p.FileName() != "" {
+				buf := bytes.NewBuffer(nil)
+				if _, err := io.Copy(buf, p); err != nil {
+					r.Status = 500
+					r.Message = err.Error()
+					return r
+				}
+
+				sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
+				if err != nil {
+					r.Status = 500
+					r.Message = err.Error()
+					return r
+				}
+
+				uploader := s3manager.NewUploader(sess)
+				_, err = uploader.Upload(&s3manager.UploadInput{
+					Bucket: bucket,
+					Key:    aws.String(filename),
+					Body:   &readSeeker{buf},
+				})
+
+				if err != nil {
+					r.Status = 500
+					r.Message = err.Error()
+					return r
+				}
+			}
+		}
+
+		status, err := bd.ModificoRegistro(usuario, IDUsuario)
+		if err != nil || !status {
+			r.Status = 400
+			r.Message = err.Error()
+			return r
+		}
+
+	} else {
+		r.Message = "Debe enviar una imagen con el 'Content-Type' de tipo 'multipart/' en el Header"
+		return r
+	}
+
+	r.Status = 200
+	r.Message = "Imagen Upload OK! "
+	return r
+
+}
