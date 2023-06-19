cad948ad39e51ce8af8b8f6880106204cbc0c6b3
livingstone23 <livingstone23@gmail.com>
2023-06-19 23:43:15 +0200

    Ultimo Commit

--- routers/uploadImage.go
+++ routers/uploadImage.go
@@ -39,9 +39,10 @@ func UploadImage(ctx context.Context, uploadType string, request events.APIGatew
 	var usuario models.Usuario
 
 	fmt.Println("funcion_UploadImage antes Bucket name1 :")
-	nombrebucket := ctx.Value(models.Key("BucketName")).(string)
-	fmt.Println("funcion_UploadImage antes Bucket name: " + nombrebucket)
-	bucket := aws.String(ctx.Value(models.Key("BucketName")).(string))
+	nombreBucket1 := "twitter-golang"
+	//nombrebucket := ctx.Value(models.Key("BucketName")).(string)
+	fmt.Println("funcion_UploadImage antes Bucket name: " + nombreBucket1)
+	bucket := aws.String(nombreBucket1)
 
 	fmt.Println("funcion_UploadImage Bucket name: ")
 
